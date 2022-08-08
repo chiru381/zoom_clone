56cdde2d4e626c7e880dca6bfabf64f83c1e5863
chiru381 <chirukosanam123@gmail.com>
2022-08-08 14:10:16 +0530

    ok

---
+++ public/script.js
@@ -0,0 +1,34 @@
+const socket = io('/')
+
+const videoGrid = document.getElementById('video-grid');
+console.log(videoGrid, '....1');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+
+let myVideoStream
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream)
+})
+
+socket.emit('join-room', ROOM_ID)
+
+socket.on('user-connected', () => {
+    connecToNewUser()
+})
+
+const connecToNewUser = () => {
+    console.log('new user connected')
+}
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video);
+}
